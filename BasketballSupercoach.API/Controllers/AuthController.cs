b33dd9acd26538451866d5241a8716c34d6c3c5a
keepitsimpletravel <ashley@keepitsimpletravel.com>
2018-11-03 17:32:18 +1000

    Authentication step #1

---
+++ BasketballSupercoach.API/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+using System.Threading.Tasks;
+using BasketballSupercoach.API.Data;
+using BasketballSupercoach.API.Dtos;
+using BasketballSupercoach.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasketballSupercoach.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            // validate request
+
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+            if(await _repo.UserExistis(userForRegisterDto.Username))
+                return BadRequest("Username already exists");
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.Username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+            return StatusCode(201);
+        }
+    }
+}
