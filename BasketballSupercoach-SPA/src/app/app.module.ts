dd4e0bbeaefdd6cc80c442fd2920a97bafa77e61
keepitsimpletravel <ashley@keepitsimpletravel.com>
2018-11-06 21:41:54 +1000

    Adding Navbar

--- BasketballSupercoach-SPA/src/app/app.module.ts
+++ BasketballSupercoach-SPA/src/app/app.module.ts
@@ -4,17 +4,21 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PlayerComponent } from './player/player.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PlayerComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      PlayerComponent,
+      NavComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
