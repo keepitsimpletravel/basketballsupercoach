a142a32a720cd9ad2f4e20d46c3582b06f6442f9
keepitsimpletravel <ashley@keepitsimpletravel.com>
2018-12-25 10:58:50 +1000

    fixing subbing issue

--- BasketballSupercoach-SPA/src/app/subplayer/subplayer.component.ts
+++ BasketballSupercoach-SPA/src/app/subplayer/subplayer.component.ts
@@ -25,22 +25,49 @@ export class SubplayerComponent implements OnInit {
   ngOnInit() {
     this.playercard = JSON.parse(this.dataRoute.snapshot.params['playercard']);
 
-    console.log(this.playercard.cardPosition + ' - the selected position');
+    console.log(this.playercard.cardPosition + ' - the selected position'); // this is correct
     // Need to get all of the player cards
     this.teamDetailService.GetPlayerCardsForUser(this.authService.decodedToken.nameid).subscribe(data => {
       // console.log('data returned for player cards length: ' + data.length);
       this.playerCards = data;
+      // console.log('length is: ' + this.playerCards.length);
       for (let p = 0; p < this.playerCards.length; p ++) {
-        // Need to check that the playercard is not the player being moved
         if (this.playerCards[p].cardPosition !== this.playercard.cardPosition) {
-          // Now we need to check if the player can player the starting positions
-          // The player is on the bench so the subbing player does not matter, the player being selected does
-          if (this.playerCards[p].playerPosition === this.playercard.cardPosition) {
-            this.subPlayers.push(this.playerCards[p]);
-            // tslint:disable-next-line:max-line-length
-            console.log('Added - ' + this.playerCards[p].firstName + ' ' + this.playerCards[p].surname + 'current position = ' + this.playerCards[p].cardPosition);
+          if (this.playercard.cardPosition <= 5) {
+            // Now need to check the other cards and whether they can play the position
+            if (this.playercard.cardPosition === this.playerCards[p].playerPosition) {
+              // Then the player can play the position
+              this.subPlayers.push(this.playerCards[p]);
+              // tslint:disable-next-line:max-line-length
+              console.log('Added - ' + this.playerCards[p].firstName + ' ' + this.playerCards[p].surname + 'current position = ' + this.playerCards[p].cardPosition);
+            }
+          } else {
+            if (this.playerCards[p].cardPosition <= 5) {
+              if (this.playercard.playerPosition === this.playerCards[p].cardPosition) {
+                this.subPlayers.push(this.playerCards[p]);
+                // tslint:disable-next-line:max-line-length
+              console.log('Added - ' + this.playerCards[p].firstName + ' ' + this.playerCards[p].surname + 'current position = ' + this.playerCards[p].cardPosition);
+              }
+            } else {
+              this.subPlayers.push(this.playerCards[p]);
+              // tslint:disable-next-line:max-line-length
+              console.log('Added - ' + this.playerCards[p].firstName + ' ' + this.playerCards[p].surname + 'current position = ' + this.playerCards[p].cardPosition);
+            }
           }
         }
+
+        // // Need to check that the playercard is not the player being moved
+        // if (this.playerCards[p].cardPosition !== this.playercard.cardPosition) {
+
+        //   // Now we need to check if the player can player the starting positions
+        //   // The player is on the bench so the subbing player does not matter, the player being selected does
+        //   if (this.playerCards[p].playerPosition === this.playercard.cardPosition) {
+        //     this.subPlayers.push(this.playerCards[p]);
+        //     // tslint:disable-next-line:max-line-length
+        // tslint:disable-next-line:max-line-length
+        //     console.log('Added - ' + this.playerCards[p].firstName + ' ' + this.playerCards[p].surname + 'current position = ' + this.playerCards[p].cardPosition);
+        //   }
+        // }
       }
       console.log('available players lenght: - ' + this.subPlayers.length);
     }, error => {
@@ -69,12 +96,6 @@ export class SubplayerComponent implements OnInit {
     const newPos = moveWith.cardPosition;
 
     console.log('orig pos: ' + originalPos + ' new pos: ' + newPos);
-    // const original
-    // Something in here is broken
-    // original.cardPosition = this.subbingPlayercard.cardPosition;
-    // original.cardPositionText = this.subbingPlayercard.cardPositionText;
-    // moveWith.cardPosition = this.playercard.cardPosition;
-    // moveWith.cardPositionText = this.playercard.cardPositionText;
     original.cardPosition = newPos;
     moveWith.cardPosition = originalPos;
 
@@ -95,38 +116,6 @@ export class SubplayerComponent implements OnInit {
       // Only once this is completed will the page go back to team
       this.router.navigate(['team/']);
     });
-
-    // // Need to write these to the database
-    // this.teamDetailService
-
-    // const positionText = this.subbingPlayercard.cardPositionText;
-
-    // const newOrigPlayer = this.subbingPlayercard;
-    // newOrigPlayer.cardPosition = this.playercard.cardPosition;
-    // newOrigPlayer.cardPositionText = this.playercard.cardPositionText;
-
-
-    // const newMovePlayer = this.playercard;
-    // newMovePlayer.cardPosition = subWith;
-    // newMovePlayer.cardPositionText = positionText;
-
-    // for (let p = 0; p < this.playerCards.length; p ++) {
-    //   if (this.playerCards[p].playerId === this.playercard.playerId) {
-    //     this.playerCards[p] = newMovePlayer;
-    //   } else if (this.playerCards[p].playerId === this.subbingPlayercard.playerId) {
-    //     this.playerCards[p] = newOrigPlayer;
-    //   }
-    // }
-
-    // Now need to write to the database
-    // for (let p = 0; p < this.playerCards.length; p ++) {
-      // this.teamDetailService.updateSubTeamDetailRecords(this.playerCards).subscribe(data => {
-      // }, error => {
-      //   this.alertify.error(error);
-      // }, () => {
-      //   this.alertify.success('Team Saved Successfully');
-      // });
-    // }
   }
 
 }
